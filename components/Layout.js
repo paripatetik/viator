bebe1b9089bd70ddd655ba29cdf01e2f1e1b442b
Maksym <saganmaksim3@gmail.com>
2026-03-21 15:32:46 +0200

    jump test 5

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col flex-1 pt-16 md:pt-20">
       <Header />
       <main className="flex-1">
         {children}
